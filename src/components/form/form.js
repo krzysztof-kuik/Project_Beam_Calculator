61d2eb1f31e7229ca56cee0c7ab478b996924325
Krzysztof Kuik <krzysztof.kuik@hotmail.com>
2019-06-20 21:48:01 +0200

    form logic modified

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -21,37 +21,97 @@ class Form extends Component {
     }, () => { this.props.getState(this.state) })
   }
 
-  numInputHandler = (e, i) => {
-    let nMinusOneEl = parseInt(this.state[`force_${i - 1}_X`])
-    let nEl = parseInt(this.state[`force_${i}_X`]);
-    let nPlusOneEl = parseInt(this.state[`force_${i + 1}_X`]);
+  lengthHandler = (e) => {
 
-    if (nPlusOneEl - nEl <= 0) {
-      console.log("war 1");
+    let xCoordinates = Object.keys(this.state).filter(element => {
+      return (element.includes("force") && element.includes("X"))
+    })
+    console.log(xCoordinates);
 
-      this.setState({
-        [e.target.name]: parseInt(e.target.value),
-        [`force_${i + 1}_X`]: parseInt(e.target.value),
-      }, () => { this.props.getState(this.state) })
+    for (let i = 0; i < xCoordinates.length; i++) {
+      const element = xCoordinates[i];
+      if (parseInt(e.target.value) <= parseInt(this.state[element])) {
+        this.setState({
+          [element]: parseInt(e.target.value),
+        }, () => { this.props.getState(this.state) })
+      }
 
     }
 
-    else {
-      console.log("war 2");
+    if (e.target.value.length === 0) {
+      console.log("war 3");
 
+      this.setState({
+        [e.target.name]: 0,
+      }, () => { this.props.getState(this.state) })
+
+      for (let i = 0; i < Object.keys(this.state).length; i++) {
+        let element = Object.keys(this.state)[i];
+
+        if (element.includes('force')) {
+          this.setState({
+            [element]: 0,
+          }, () => { this.props.getState(this.state) })
+        }
+
+      }
+    }
+
+
+    else {
       this.setState({
         [e.target.name]: parseInt(e.target.value),
       }, () => { this.props.getState(this.state) })
     }
   }
 
+  numInputHandler = (e, i) => {
+    let nMinusOneEl = parseInt(this.state[`force_${i - 1}_X`])
+    let nEl = parseInt(this.state[`force_${i}_X`]);
+    let nPlusOneEl = parseInt(this.state[`force_${i + 1}_X`]);
+    let maximum = this.state.beamLength;
+
+    console.log(e.target.value);
+    console.log(typeof e.target.value);
+    console.log(e.target.value.length);
+
+    if (e.target.value.length === 0) {
+      console.log("war 3");
+
+      this.setState({
+        [e.target.name]: 0,
+      }, () => { this.props.getState(this.state) })
+    }
+
+    else if (nPlusOneEl - nEl <= 0 && e.target.value < maximum) {
+      console.log("war 1");
+
+      this.setState({
+        [e.target.name]: parseInt(e.target.value),
+        [`force_${i + 1}_X`]: parseInt(e.target.value) + 1,
+      }, () => { this.props.getState(this.state) })
+
+    }
+
+    else if (e.target.value <= maximum) {
+      console.log("war 2");
+
+      this.setState({
+        [e.target.name]: parseInt(e.target.value),
+      }, () => { this.props.getState(this.state) })
+    }
+
+
+  }
+
 
   render() {
 
     let forceForms = [];
+    let errorInfo = [];
     for (let i = 1; i <= this.state.numOfForces; i++) {
 
-      let minXValue = i >= 2 ? parseInt(this.state[`force_${i - 1}_X`]) + 1 : 0;
+      let minXValue = i >= 2 ? parseInt(this.state[`force_${i - 1}_X`]) + 0 : 0;
 
       let maxValue = this.state.beamLength;
       let xCoordValue = this.state[`force_${i}_X`] <= maxValue ? this.state[`force_${i}_X`] : maxValue;
@@ -64,16 +124,24 @@ class Form extends Component {
           </label>
 
           <label> Please specify x coordinate of Force Number {i}
-            <input onChange={e => this.numInputHandler(e, i)} name={`force_${i}_X`} type="number" value={xCoordValue} min={minXValue} max={this.state.beamLength} />
+            <input onChange={e => this.numInputHandler(e, i)} name={`force_${i}_X`} type="number" value={xCoordValue} min={minXValue} max={maxValue} />
           </label>
         </div>
       )
+
+      if (parseInt(this.state[`force_${i - 1}_X`]) > parseInt(this.state[`force_${i}_X`])) {
+        errorInfo.push(
+          <div>
+            Value of Force {i} should be greater than Force {i - 1}
+          </div>
+        )
+      }
     }
 
     return (
       <form>
         <label> Please specify beam length
-            <input onChange={this.numInputHandler} name="beamLength" type="number" min="0" value={this.state.beamLength} />
+            <input onChange={this.lengthHandler} name="beamLength" type="number" min="0" value={this.state.beamLength} />
         </label>
         <div>
           Please specify number of forces
@@ -90,6 +158,7 @@ class Form extends Component {
         {(this.state.numOfForces > 0 && this.state.beamLength > 0) &&
           forceForms
         }
+        {errorInfo}
 
       </form>
     )
