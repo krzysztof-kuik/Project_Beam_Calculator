a0af7814d656c20fdf6b1df99b1ceff3a8a3fcd2
Krzysztof Kuik <krzysztof.kuik@hotmail.com>
2019-06-21 13:04:36 +0200

    beam preview styling

--- src/components/beam_preview/beam_preview.js
+++ src/components/beam_preview/beam_preview.js
@@ -4,54 +4,61 @@ import '../../assets/variables.scss';
 
 class BeamPreview extends Component {
 
-    render() {
-        // console.log(this.props);
-        // let { numOfForces, force1X, force2X, force1Val, force2Val } = this.props;
-        let previewWidth = 800;
-        let previewWindowStyle = { width: `${previewWidth}px` }
-        let forces = [];
-        for (let i = 1; i <= this.props.numOfForces; i++) {
-
-            let forceStyle = {
-                width: this.props[`force${i}X`],
-                height: `${130 + (i - 1) * 60}px`,
-                borderRight: this.props[`force${i}X`] >= 60 ? "2px solid black" : "0 solid black"
-            }
-
-            if ((this.props[`force${i + 1}X`] !== undefined && this.props[`force${i}X`] <= this.props[`force${i + 1}X`]) || (this.props[`force${i + 1}X`] === undefined && this.props[`force${i}X`] >= this.props[`force${i - 1}X`])) {
-                forces.push(
-                    <>
-                        <div className="forceContainer" style={forceStyle}>
-                            {
-                                this.props[`force${i}X`] >= 60 &&
-                                <div className="beamContainer__dimensionArrow">{this.props[`force${i}X`]}</div>
-
-                            }
-                            <div className="forceContainer__force">
-                                <div className={`forceContainer__badge no${i}`}>Force {i}</div>
-                            </div>
-                        </div>
-                    </>
-                )
-            }
-        }
-
-        return (
-            <>
-                <h2>BEAM PREVIEW</h2>
-                <div className="beamPreview">
-                    <div className="beamContainer"
-                        style={previewWindowStyle}>
-                        <div className="beamContainer__dimensionArrow">Beam length: {this.props.beamLength}</div>
-                        {forces}
-                        <div className="beamContainer__beam"></div>
+  render() {
+    // console.log(this.props);
+    // let { numOfForces, force1X, force2X, force1Val, force2Val } = this.props;
+    let previewWidth = 800;
+    let previewWindowStyle = { width: `${previewWidth}px` }
+    let forces = [];
 
 
-                    </div>
-                </div>
-            </>
+    for (let i = 1; i <= this.props.numOfForces; i++) {
+
+      let forceXcoordinate = (this.props[`force${i}X`] / this.props.beamLength) * previewWidth;
+      let showDimensionLine = forceXcoordinate >= 60;
+
+      let forceStyle = {
+        width: forceXcoordinate,
+        height: `${130 + (i - 1) * 60}px`,
+        borderRight: showDimensionLine ? "2px solid black" : "0 solid black"
+      }
+
+      if ((this.props[`force${i + 1}X`] !== undefined && this.props[`force${i}X`] <= this.props[`force${i + 1}X`]) || (this.props[`force${i + 1}X`] === undefined && this.props[`force${i}X`] >= this.props[`force${i - 1}X`])) {
+        forces.push(
+          <>
+            <div className="forceContainer" style={forceStyle}>
+              {
+                showDimensionLine &&
+                <div className="beamContainer__dimensionArrow">{this.props[`force${i}X`]}</div>
+
+              }
+              <div className={`forceContainer__force force_${i}`}>
+                <div className={`forceContainer__badge no${i}`}>Force {i}</div>
+              </div>
+            </div>
+          </>
         )
+      }
     }
+
+    return (
+      <>
+        <h2>BEAM PREVIEW</h2>
+        <div className="beamPreview">
+          <div className="beamContainer"
+            style={previewWindowStyle}>
+            <div className="beamContainer__dimensionArrow">Beam length: {this.props.beamLength}</div>
+            {forces}
+            <div className="beamContainer__beam"></div>
+          </div>
+        </div>
+      </>
+    )
+  }
 }
 
 export default BeamPreview
+
+
+
+
