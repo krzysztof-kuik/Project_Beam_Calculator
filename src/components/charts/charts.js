7185590c620059d6e6707fb6e52b2859e49414cc
Krzysztof Kuik <krzysztof.kuik@hotmail.com>
2019-06-22 17:10:11 +0200

    charts section in progress

--- src/components/charts/charts.js
+++ src/components/charts/charts.js
@@ -1,73 +1,97 @@
 import React, { Component } from 'react'
 import './charts.scss';
-import { LineChart, Line } from 'recharts';
+import {
+	LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 
 class Charts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      reactionA: 0,
-      reactionB: 0
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			reactionA: 0,
+			reactionB: 0
+		}
+	}
 
-  dataProcessor = () => {
-    let { numOfForces, beamLength, force1X, force2X, force1Val, force2Val } = this.props;
-    let { reactionA, reactionB } = this.state;
-    let firstSectionData = {};
-    console.log((force1X / 10).toFixed(1));
+	dataPreprocessor = () => {
+		let { numOfForces, beamLength, force1X, force2X, force1Val, force2Val } = this.props;
+		let { reactionA, reactionB } = this.state;
+		let data = [];
+		console.log((force1X / 10).toFixed(1));
 
-    for (let i = 0.0; parseFloat((i).toFixed(1)) <= force1X; i += parseFloat((force1X / 10).toFixed(1))) {
+		for (let i = 0.0; parseFloat((i).toFixed(1)) < force1X; i += parseFloat((force1X / 10).toFixed(1))) {
+			let index = parseFloat((i).toFixed(1));
 
-      let index = parseFloat((i).toFixed(1));
-      firstSectionData[index] = reactionA * i;
-    }
-    console.log(firstSectionData);
+			data.push(
+				{
+					x: index,
+					torque: parseFloat((reactionA * i).toFixed(1))
+				}
+			)
+			// data[index] = parseFloat((reactionA * i).toFixed(1));
+		}
+		console.log(data);
 
-    let border = numOfForces < 2 ? beamLength : force2X;
+		let border = numOfForces < 2 ? beamLength : force2X;
 
-    for (let i = parseFloat(force1X); parseFloat((i).toFixed(1)) <= border; i += parseFloat(((border - force1X) / 10).toFixed(1))) {
+		for (let i = parseFloat(force1X); parseFloat((i).toFixed(1)) <= border; i += parseFloat(((border - force1X) / 10).toFixed(1))) {
 
-      console.log(i);
+			console.log(i);
 
-      let index = parseFloat((i).toFixed(1));
-      firstSectionData[index] = reactionA * i - force1Val * (i - force1X);
-    }
-    console.log(firstSectionData);
+			let index = parseFloat((i).toFixed(1));
+
+			data.push(
+				{
+					x: index,
+					torque: parseFloat((reactionA * i - force1Val * (i - force1X)).toFixed(1))
+				}
+			)
+			// data[index] = parseFloat((reactionA * i - force1Val * (i - force1X)).toFixed(1));
+		}
+		console.log(data);
+		this.setState({
+			data: data
+		})
 
 
-  }
+	}
 
-  reactionsCalc = () => {
-    let { beamLength, force1X, force2X, force1Val, force2Val } = this.props;
-    console.log(beamLength);
+	reactionsCalc = () => {
+		let { beamLength, force1X, force2X, force1Val, force2Val } = this.props;
+		console.log(beamLength);
 
-    let reactionA = (force1Val * (beamLength - force1X) + force2Val * (beamLength - force2X)) / beamLength;
+		let reactionA = (force1Val * (beamLength - force1X) + force2Val * (beamLength - force2X)) / beamLength;
 
-    let reactionB = (force1Val * force1X + force2Val * force2X) / beamLength;
+		let reactionB = (force1Val * force1X + force2Val * force2X) / beamLength;
 
-    this.setState({
-      reactionA: reactionA,
-      reactionB: reactionB
-    }, this.dataProcessor)
-  }
+		this.setState({
+			reactionA: reactionA,
+			reactionB: reactionB
+		}, this.dataPreprocessor)
+	}
 
-  render() {
-    console.log(this);
-    // console.log(this.props);
-    return (
-      <section className="chartsSection">
-        <button onClick={this.reactionsCalc}>RESOLVE</button>
-        <div className="chartsContainer">
-          <LineChart
-            width="400" height="200"
-          >
-
-          </LineChart>
-        </div>
-      </section>
-    )
-  }
+	render() {
+		console.log(this);
+		// console.log(this.props);
+		return (
+			<section className="chartsSection">
+				<button onClick={this.reactionsCalc}>RESOLVE</button>
+				<div className="chartsContainer">
+					<LineChart
+						width={860} height={400} data={this.state.data}
+						margin={{ top: 5, right: 0, left: 0, bottom: 5 }}
+					>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="x" />
+						<YAxis />
+						<Tooltip />
+						<Legend />
+						<Line type="monotone" dataKey="torque" stroke="#8884d8" activeDot={{ r: 8 }} />
+					</LineChart>
+				</div>
+			</section>
+		)
+	}
 }
 
 export default Charts;
